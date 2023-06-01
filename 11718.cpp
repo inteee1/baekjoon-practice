bcf8e53fa420b02e2001bfd1c919d0216334b06a
inteee1 <131097590+inteee1@users.noreply.github.com>
2023-06-01 11:12:45 +0900

    Add files via upload

---
+++ 11718.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string a;
+	while (true)
+	{
+		getline(cin, a);
+		if (a == "") { break; }
+		cout << a << endl;
+	}
+
+	return 0;
+}
