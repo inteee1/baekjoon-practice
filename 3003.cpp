e887f1d126207357288584f3127ef09619511475
inteee1 <131097590+inteee1@users.noreply.github.com>
2023-06-02 14:15:57 +0900

    Add files via upload

---
+++ 3003.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main(void)
+{
+	int array[10] = { 1, 1, 2, 2, 2, 8 };
+	int a;
+
+	for (int i = 0; i < 6; ++i)
+	{
+		cin >> a;
+		cout << array[i] - a << " ";
+	}
+	cout << endl;
+	return 0;
+}
---
+++ 3003.py
@@ -0,0 +1,7 @@
+array = [ 1, 1, 2, 2, 2, 8] 
+
+a = list(map(int, input().split()))
+
+for i in range(6):
+    print(array[i] - a[i], end=' ')
+    
