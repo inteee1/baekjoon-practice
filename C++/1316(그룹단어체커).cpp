05a681c5e357c68df8b936772cdbc41371cc7bb7
inteee1 <131097590+inteee1@users.noreply.github.com>
2023-06-07 12:05:17 +0900

    Add files via upload

---
+++ C++/1316(그룹단어체커).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string S;
+	int n;
+	cin >> n;
+
+	int count = n;
+	for (int i = 0; i < n; ++i) {
+		char first = '0';
+		int A[26] = { 0, };
+		cin >> S;
+		for (int j = 0; S[j] != '\0'; ++j) {
+			if (first != S[j]) {
+				first = S[j];
+				A[S[j] - 'a'] += 1;
+			}
+			if (A[S[j] - 'a'] == 2) {
+				count -= 1;
+				break;
+			}
+		}
+
+	}
+	cout << count << endl;
+	
+
+
+	return 0;
+}
---
+++ C++/25206(너의평점은).cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+
+using namespace std;
+float getscore(char* G)
+{
+	if (G[0] == 'A')
+	{
+		if (G[1] == '+') {
+			return 4.5;
+		}
+		else if (G[1] == '0') {
+			return 4.0;
+		}
+	}
+	else if (G[0] == 'B')
+	{
+		if (G[1] == '+') {
+			return 3.5;
+		}
+		else if (G[1] == '0') {
+			return 3.0;
+		}
+	}
+	else if (G[0] == 'C')
+	{
+		if (G[1] == '+') {
+			return 2.5;
+		}
+		else if (G[1] == '0') {
+			return 2.0;
+		}
+	}
+	else if (G[0] == 'D')
+	{
+		if (G[1] == '+') {
+			return 1.5;
+		}
+		else if (G[1] == '0') {
+			return 1.0;
+		}
+	}
+	return 0;
+
+
+}
+
+
+
+
+
+
+int main(void)
+{
+	char S[100000];
+	char G[3];
+	float H, sum = 0.0;
+	double AS = 0.0;
+
+	for (int i = 0; i < 20; ++i)
+	{
+		cin >> S >> H >> G;
+		if (G[0] != 'P') {
+			sum += H;
+			AS += H * getscore(G);
+		}
+	}
+	cout.precision(6);
+	cout << fixed;
+	cout << AS / sum << endl;
+	
+
+	return 0;
+}
