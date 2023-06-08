415144f8b2d2b0f4d0055b93d6d42a93ac67b68b
inteee1 <131097590+inteee1@users.noreply.github.com>
2023-06-08 11:00:09 +0900

    Add files via upload

---
+++ C++/7-1 2738(행렬과 덧셈).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, N;
+	cin >> n >> N;
+
+
+	int A[101][101];
+	int B[101][101];
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < N; ++j) {
+			cin >> A[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < N; ++j) {
+			cin >> B[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < N; ++j) {
+			int sum = A[i][j] + B[i][j];
+			cout << sum << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
